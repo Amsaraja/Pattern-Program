aa48dedc8b436d8e4485ba2422b3c14698f389d5
Amsaraja <161423867+Amsaraja@users.noreply.github.com>
2024-07-21 13:56:39 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -1,5 +1,5 @@
-n=int(input("Enter the Number of rows: "))
-for i in range(0, n):  
+a=int(input("Enter the Number of rows: "))
+for i in range(0, a):  
         for j in range(0, i + 1):  
             print("* ", end=" ")        
         print()
