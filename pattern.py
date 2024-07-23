97507a119aafc064a969de071a2c33aeb96e6a8b
Amsaraja <amsarajadeivam@gmail.com>
2024-07-23 12:56:09 +0530

    variable

--- pattern.py
+++ pattern.py
@@ -1,5 +1,5 @@
-a=int(input("Enter the Number of rows: "))
-for i in range(0, a):  
+b=int(input("Enter the Number of rows: "))
+for i in range(0, b):  
         for j in range(0, i + 1):  
             print("* ", end=" ")        
         print()
