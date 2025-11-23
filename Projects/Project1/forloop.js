0d5170959e40466549f5c8589343c9c14bdc808b
Amsaraja <161423867+Amsaraja@users.noreply.github.com>
2025-11-23 16:37:12 +0530

    Add files via upload

---
+++ Projects/Project1/forloop.js
@@ -0,0 +1,20 @@
+// for (let i=1;i<=100;i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+
+// let i=1
+// while(i<=10){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+do{
+    console.log("Hello World");
+    i++;
+
+}
+while(i<=10);
